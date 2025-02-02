a1109f7463ad93714d92f8ea96fb8d0838f82128
Faiyaz Awsaf <faiyazawsaf11@gmail.com>
2025-02-02 23:10:15 +0600

    set project structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
 const app = express();
+const tasks = require('./routes/tasks.js');
 
+//middleware
 app.use(express.json());
 
+//routes
+app.get('/home', (req, res) => {
+    res.send("Task Manager App");
+})
 
+app.use('/api/v1/tasks', tasks);
+
+
+//server concetion
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
